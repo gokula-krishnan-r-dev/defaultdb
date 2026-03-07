5778a270ed28a5ad843023719c71d2b19e7f582a
gokula <gokulakrishnanr812@gmail.com>
2026-03-07 17:21:47 +0530

    Demo

---
+++ defaultdb/queries/table_sizes.sql
@@ -0,0 +1,5 @@
+SELECT schemaname, tablename,
+  pg_size_pretty(pg_total_relation_size(schemaname||'.'||tablename)) AS size
+FROM pg_tables
+WHERE schemaname NOT IN ('pg_catalog','information_schema')
+ORDER BY pg_total_relation_size(schemaname||'.'||tablename) DESC;
